b34811eae185d0d03e1f96b90c3c30278dcd45a7
YangFish <16751870@qq.com>
2023-12-29 18:18:28 +0800

    Update strap.c

--- kernel/strap.c
+++ kernel/strap.c
@@ -59,7 +59,7 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
       //panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
-      if (stval > USER_STACK_TOP && (USER_STACK_TOP - stval) >= 20 * (STACK_SIZE)) {
+      if (stval < USER_STACK_TOP && stval > (USER_STACK_TOP - 20 * STACK_SIZE)) {
         user_vm_map((pagetable_t)current->pagetable, 
           stval - stval % PGSIZE, PGSIZE, 
           (uint64)alloc_page(), prot_to_type(PROT_WRITE | PROT_READ, 1));
